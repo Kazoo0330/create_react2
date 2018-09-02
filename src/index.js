e98727c623bfae2251fc970dd6951878e0616667
kazoo <kazuki_saito@diveintocode.jp>
2018-09-02 13:50:47 +0900

    feat: lecture25 completed.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const array1 = [0, 1, 2, 3, 4];
 
-const newArray1 = array1.map((output, index) => {
-  return `${index} is ${output}.`;
+const newArray1 = array1.filter((output, index) => {
+  return output > 3
 });
 
 console.log(newArray1);
 
 const array2 = ['アムロ', 'ハヤト', 'カイ', 'リュウ', 'ブライト'];
 
-const newArray2 = array2.map((output, index) => {
-  return `${index}番目は${output}さんです。`;
+const newArray2 = array2.filter((output, index) => {
+  return output === 'アムロ';
 });
 
 console.log(newArray2);
+
+const newArray3 = array2.filter((output, index) => {
+  return output.length > 3;
+});
+
+console.log(newArray3);
